b88e313be5a069286ed02cd777a7f047357d01dc
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-03-13 00:00:46 +0100

    initial commit

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:setemiojo@gmail.com">setemiojo@gmail.com</a>
+    </p>
+  </div>
+);
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+
+export default () => (
+  <div>
+    <h1>Hi! I'm Oluwasetemi and I welcome you to my blog!</h1>
+    <p>I will be writing about my interests and what I do every day! Please enjoy the ride. </p>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </div>
+);
