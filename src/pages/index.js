c7b56a0d1794d0fec12de45cc967bb2e023bb674
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-03-13 00:59:57 +0100

    before changing the index.js to blog format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import Link from 'gatsby-link'
-
+import React from "react";
 
 export default () => (
   <div>
-    <h1>Hi! I'm Oluwasetemi and I welcome you to my blog!</h1>
-    <p>I will be writing about my interests and what I do every day! Please enjoy the ride. </p>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
+    <h1>Amazing Pandas Eating Things</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
+    </div>
   </div>
 );
